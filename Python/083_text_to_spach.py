0c032c922bdc732feca8e2eb1b47cac04f5efde2
Varun <Varun.warudkar@gmail.com>
2023-07-05 20:34:07 +0530

    updated text to speach

--- Python/083_text_to_spach.py
+++ Python/083_text_to_spach.py
@@ -8,7 +8,21 @@
 #     os.system(f"good_{name}.mp3")
 #     sleep(2)
 
-import pyttsx
-e = pyttsx.init()
-e.say('Good morning.')
-e.runAndWait()
+# import pyttsx
+# e = pyttsx.init()
+# e.say('Good morning.')
+# e.runAndWait()
+from os import name as Name
+print(Name)
+# Windows
+import win32com.client #pip install pywin32
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+names = ["Heramb","Vrinda","Renuka","Swara","Samihan"]
+for name in names:
+    speaker.Speak(f"HI {name} , lots of shout out for {name}, Hip Hip Hurray")
+
+# # for Mac
+# from os import system
+# names = ["Heramb","Vrinda","Renuka","Swara","Samihan"]
+# for name in names:
+#     system(f'HI {name} , lots of shout out for {name}, Hip Hip Hurray')
